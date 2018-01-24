a906bc62e1d65d5f3e52813f2a9eebe8bec2dd2f
yywing <386542536@qq.com>
2018-01-24 23:04:59 +0800

    Update and rename 两人随机发牌比大小 to two_people_random.py

--- two_people_random.py
+++ two_people_random.py
@@ -1,16 +1,16 @@
 poker = ['2S','2C','2D','2H',
-         '3S','3C','3D','3H',
-		 '4S','4C','4D','4H',
-		 '5S','5C','5D','5H',
-		 '6S','6C','6D','6H',
-		 '7S','7C','7D','7H',
-		 '8S','8C','8D','8H',
-		 '9S','9C','9D','9H',
-		 'OS','OC','OD','OH',
-		 'JS','JC','JD','JH',
-		 'QS','QC','QD','QH',
-		 'KS','KC','KD','KH',
-		 'AS','AC','AD','AH']
+        '3S','3C','3D','3H',
+        '4S','4C','4D','4H',
+        '5S','5C','5D','5H',
+        '6S','6C','6D','6H',
+        '7S','7C','7D','7H',
+        '8S','8C','8D','8H',
+        '9S','9C','9D','9H',
+        'OS','OC','OD','OH',
+        'JS','JC','JD','JH',
+        'QS','QC','QD','QH',
+        'KS','KC','KD','KH',
+        'AS','AC','AD','AH']
 import random
 hand1 = random.sample(poker,5)
 hand2 = random.sample(poker,5)
